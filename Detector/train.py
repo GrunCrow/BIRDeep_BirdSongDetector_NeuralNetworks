74ebcccbaf7f81d376c3ca7aea5dff0703bdb810
GrunCrow <albamrqz751@gmail.com>
2024-06-04 18:02:34 +0000

    Experiments

--- Detector/train.py
+++ Detector/train.py
@@ -17,7 +17,7 @@ os.environ["COMET_LOG_PER_CLASS_METRICS"] = "true"
 os.environ["COMET_MAX_IMAGE_PREDICTIONS"] = "50"
 
 # initialize experiment in comet
-comet_ml.init("BIRDeep") # it get the name of the project name on training
+#comet_ml.init("BIRDeep") # it get the name of the project name on training
 
 # Create a new YOLO model from scratch
 #model = YOLO(MODEL_NAME)
@@ -26,26 +26,60 @@ comet_ml.init("BIRDeep") # it get the name of the project name on training
 #model = YOLO(MODEL_WEIGHTS)
 
 # Nano model
-model = YOLO("yolov8x.pt")
+#model = YOLO("yolov8s.pt")
 
-# Train the model using the 'coco128.yaml' dataset for 3 epochs
-model.train(
+# Grid search optimization
+optimizers = ["SGD", "Adam", "RMSProp"]
+lr0_values = [0.001, 0.01, 0.1]
+momentum_values = [0.9, 0.937, 0.99]
+weight_decay_values = [0.0001, 0.0005, 0.001]
+
+for optimizer in optimizers:
+    for lr0 in lr0_values:
+        for momentum in momentum_values:
+            for weight_decay in weight_decay_values:
+                print("====================================== EXPERIMENT ======================================")
+                print("Optimizer: ", optimizer)
+                print("lr0: ", lr0)
+                print("momentum: ", momentum)
+                print("weight_decay: ", weight_decay)
+                print("==========================================================================================")
+
+                comet_ml.init("BIRDeep") # it get the name of the project name on training
+
+                model = YOLO("yolov8s.pt")
+
+                # Train the model using the 'coco128.yaml' dataset for 3 epochs
+                model.train(
                     data=DATASET_YAML, 
                     device = 0,                   # device to run on, i.e. cuda device=0 or device=0,1,2,3 or device=cpu
                     epochs = 500,
-                    patience = 30,
-                    name = "5_Binary_NoNoAudio_Extreme", # "2_BaseExperimentBinary_Small",      # experiment name
+                    patience = 50,
+                    name = "6_Binary_" + optimizer + "_lr0" + str(lr0) + "_momentum" + str(momentum) + "_wd" + str(weight_decay) + "_Small", # "2_BaseExperimentBinary_Small",      # experiment name
                     resume = False,	            # resume training from last checkpoint
                     single_cls = True,	        # train multi-class data as single-class -> def = False
                     cfg="Detector/config/config.yaml",
                     pretrained=True,
+
+                    optimizer = optimizer, # (str) optimizer to use, choices=[SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto]
+                    
                     cos_lr = True,
+                    lr0 = lr0, # (float) initial learning rate (i.e. SGD=1E-2, Adam=1E-3)
+                    lrf = 0.01, # (float) final learning rate (lr0 * lrf)
+                    momentum = momentum, # (float) SGD momentum/Adam beta1
+                    weight_decay = weight_decay, # (float) optimizer weight decay 5e-4
+                    warmup_epochs = 3.0, # (float) warmup epochs (fractions ok)
+                    warmup_momentum = 0.8, # (float) warmup initial momentum
+                    warmup_bias_lr = 0.1, # (float) warmup initial bias lr
+                    box = 7.5, # (float) box loss gain
+                    cls = 0.5, # (float) cls loss gain (scale with pixels)
+                    dfl = 1.5 # (float) dfl loss gain
                     )
 
-model.val(
+'''model.val(
     split = "val", # val, test or train
     conf = 0.4,  # confidence threshold
-)
+)'''
 
 # What Manuel wanted :´) -> for classification model, will it work with detection???????????????????
 '''# Run inference on an image
